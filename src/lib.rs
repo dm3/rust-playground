44d97229bbcffe95c228cb583fc48df1d1ed418a
Vadim Platonov <vadim.platonov@tuloscapital.com>
2016-11-18 11:34:13 +0100

    remove tokio

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,17 @@
 
 extern crate libc;
 
-extern crate serde;
-extern crate serde_json;
-
-#[macro_use]
-extern crate serde_derive;
+// extern crate serde;
+// extern crate serde_json;
+// #[macro_use]
+// extern crate serde_derive;
 
 extern crate futures;
-extern crate tokio_core;
-extern crate tokio_proto;
-extern crate tokio_service;
 
 #[macro_use]
 extern crate log;
 
-extern crate mio;
 extern crate ws;
 
 pub mod callbacks;
-pub mod tokio;
 pub mod ws_test;
