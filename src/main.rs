206df78671b7b6ad518cceca5041f830627c0a15
Vadim Platonov <vadim.platonov@tuloscapital.com>
2016-11-18 11:34:22 +0100

    neater WS client

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,27 @@ extern crate ws;
 use std::thread;
 use playground::ws_test;
 
+fn start_client(addr: &'static str, name: &'static str) {
+    ws::connect(addr, |out| {
+        out.send("hi!").unwrap();
+        move |msg| {
+            println!("Client {} got msg {}!", name, msg);
+            Ok(())
+        }
+    }).expect("Could not connect!");
+}
+
 fn main() {
     env_logger::init().unwrap();
     println!("> starting up...");
     ws_test::start("127.0.0.1:3100", 0);
     thread::sleep_ms(100);
     println!("> started threads...");
-    ws::connect("ws://127.0.0.1:3100", |out| {
-        out.send("hi!").unwrap();
-        move |msg| {
-            println!("Client got msg {}!", msg);
-            Ok(())
-        }
-    }).expect("Could not connect!");
+    let a = thread::spawn(move || { start_client("ws://127.0.0.1:3100", "A") });
+    println!("> started client A...");
+    thread::sleep_ms(5000);
+    let b = thread::spawn(move || { start_client("ws://127.0.0.1:3100", "B") });
+    println!("> started client B...");
+    a.join();
+    b.join();
 }
